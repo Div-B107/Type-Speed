36db1cdf5ace532adc3dba62a421c57ec5c186b9
Elcan Mahmudov <elcan.m0707@gmail.com>
2023-09-16 19:09:14 +0400

    Added random word method

--- Type-Speed/src/Main.java
+++ Type-Speed/src/Main.java
@@ -1,5 +1,20 @@
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String urlPath = "https://random-word-api.herokuapp.com/word";
+        try {
+            URL url = new URL(urlPath);
+            Scanner scanner = new Scanner(url.openStream());
+            String randomWord = scanner.useDelimiter("\\A").nextLine();
+            System.out.println(randomWord);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
     }
 }
