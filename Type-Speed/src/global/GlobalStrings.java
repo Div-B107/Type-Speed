cce55b63dbe0e5a3a0329bd67b31c8d019505037
Elcan Mahmudov <elcan.m0707@gmail.com>
2023-09-23 16:43:22 +0400

    Write to file methods added

---
+++ Type-Speed/src/global/GlobalStrings.java
@@ -0,0 +1,9 @@
+package global;
+
+public class GlobalStrings {
+
+
+    public static final String FILE_NAME = "random-words.txt";
+    public static final String URL_PATH = "https://random-word-api.herokuapp.com/word";
+
+}
